4adfb1fef5df98c053aafd8225d74b9a47f5973d
David CZ <dacaza@gmail.com>
2022-12-06 12:10:02 +0100

    day 6 completed

--- AoC2022/src/AoC_day06.h
+++ AoC2022/src/AoC_day06.h
@@ -4,7 +4,7 @@
 class AoC22_06 : public AoC22
 {
 private:
-	std::vector<int> data;
+	std::string data;
 
 public:
 	AoC22_06(const std::string& Filename) : AoC22(Filename)
@@ -17,22 +17,61 @@ private:
 	{
 		data.clear();
 		for (std::string line : this->rawData)
-		{
-
-		}
+			data = line;
 	}
 
 	std::string part1() override
 	{
+		const int marker_len = 4;
+		int k = this->getMarker(marker_len);
+
+		std::string marker = data.substr(k - marker_len, marker_len);
 		
 		std::cout << "AoC 2022 - Day 06 - Part 1";
-		return "R1";
+		return marker + " " + std::to_string(k);
 	}
 
 	std::string part2() override
 	{
+		const int marker_len = 14;
+		int k = this->getMarker(marker_len);
+
+		std::string marker = data.substr(k - marker_len, marker_len);
+
 		std::cout << "AoC 2022 - Day 06 - Part 2";
-		return "R2";
+		return marker + " " + std::to_string(k);
+	}
+
+	int getMarker(const int& marker_len)
+	{
+		std::string marker;
+
+		int k = 0;
+		while (k < data.length())
+		{
+			marker = data.substr(k, marker_len);
+
+			bool stop = false;
+			int i = 0;
+			while (i < marker_len && !stop)
+			{
+				int j = i + 1;
+				while (j < marker_len && !stop)
+				{
+					if (marker[i] == marker[j])
+						stop = true;
+					j++;
+				}
+				i++;
+			}
+			if (!stop)
+				break;
+			k++;
+		}
+
+		return k + marker_len;
 	}
 
 };
+
+
--- AoC2022/src/main.cpp
+++ AoC2022/src/main.cpp
@@ -1,19 +1,18 @@
-#include "AoC_day01.h"
-#include "AoC_day02.h"
-#include "AoC_day03.h"
-#include "AoC_day04.h"
-#include "AoC_day05.h"
+//#include "AoC_day01.h"
+//#include "AoC_day02.h"
+//#include "AoC_day03.h"
+//#include "AoC_day04.h"
+//#include "AoC_day05.h"
 #include "AoC_day06.h"
 
 int main()
 {
 	std::string path = "C:/Users/dacaz/source/repos/AoC2022/AoC2022/data/";
 
-	AoC22_01 day01(path + "data01.txt");
-	AoC22_02 day02(path + "data02.txt");
-	AoC22_03 day03(path + "data03.txt");
-	AoC22_04 day04(path + "data04.txt");
-	AoC22_05 day05(path + "data05.txt");
+	//AoC22_01 day01(path + "data01.txt");
+	//AoC22_02 day02(path + "data02.txt");
+	//AoC22_03 day03(path + "data03.txt");
+	//AoC22_04 day04(path + "data04.txt");
+	//AoC22_05 day05(path + "data05.txt");
 	AoC22_06 day06(path + "data06.txt");
-
 }

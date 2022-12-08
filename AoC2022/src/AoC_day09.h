5177c4d5b55cac1b2ed187b89b0dcbd844db1ebc
David CZ <dacaza@gmail.com>
2022-12-08 16:50:31 +0100

    day 9 ready

---
+++ AoC2022/src/AoC_day09.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "AoC22.h"
+
+class AoC22_09 : public AoC22
+{
+private:
+	std::vector<int> data;
+
+public:
+	AoC22_09(const std::string& Filename) : AoC22(Filename)
+	{
+		this->solve();
+	}
+
+private:
+	void loadData() override
+	{
+		data.clear();
+		for (std::string line : this->rawData)
+		{
+
+		}
+	}
+
+	std::string part1() override
+	{
+		std::cout << "AoC 2022 - Day 09 - Part 1";
+		return "R1";
+	}
+
+	std::string part2() override
+	{
+		std::cout << "AoC 2022 - Day 09 - Part 2";
+		return "R2";
+	}
+
+};
--- AoC2022/src/main.cpp
+++ AoC2022/src/main.cpp
@@ -6,6 +6,7 @@
 //#include "AoC_day06.h"
 //#include "AoC_day07.h"
 #include "AoC_day08.h"
+#include "AoC_day09.h"
 
 int main()
 {
@@ -19,4 +20,5 @@ int main()
 	//AoC22_06 day06(path + "data06.txt");
 	//AoC22_07 day07(path + "data07.txt");
 	AoC22_08 day08(path + "data08.txt");
+	AoC22_09 day09(path + "data09.txt");
 }

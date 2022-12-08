92d9ca85e1a1b6281da43e72b659efd2db0e46d1
David CZ <dacaza@gmail.com>
2022-12-08 16:29:09 +0100

    draw for day 08

--- AoC2022/src/AoC_day08.h
+++ AoC2022/src/AoC_day08.h
@@ -5,6 +5,12 @@ class AoC22_08 : public AoC22
 {
 private:
 	std::vector<std::vector<int>> data;
+	std::vector<std::vector<bool>> mVisible;
+	std::vector<std::vector<bool>> mVisible_top;
+	std::vector<std::vector<bool>> mVisible_bottom;
+	std::vector<std::vector<bool>> mVisible_left;
+	std::vector<std::vector<bool>> mVisible_right;	
+	std::vector<std::vector<int>> mScenic_score;
 
 public:
 	AoC22_08(const std::string& Filename) : AoC22(Filename)
@@ -18,6 +24,30 @@ private:
 		for (int i = 0; i < data.size(); i++)
 			data[i].clear();
 		data.clear();
+
+		for (int i = 0; i < mVisible.size(); i++)
+			mVisible[i].clear();
+		mVisible.clear();
+
+		for (int i = 0; i < mVisible_top.size(); i++)
+			mVisible_top[i].clear();
+		mVisible_top.clear();
+
+		for (int i = 0; i < mVisible_bottom.size(); i++)
+			mVisible_bottom[i].clear();
+		mVisible_bottom.clear();
+
+		for (int i = 0; i < mVisible_left.size(); i++)
+			mVisible_left[i].clear();
+		mVisible_left.clear();
+
+		for (int i = 0; i < mVisible_right.size(); i++)
+			mVisible_right[i].clear();
+		mVisible_right.clear();
+
+		for (int i = 0; i < mScenic_score.size(); i++)
+			mScenic_score[i].clear();
+		mScenic_score.clear();
 
 		data.resize(rawData.size());
 		for (int i = 0; i < this->rawData.size(); i++)
@@ -45,8 +75,16 @@ private:
 		//	35390
 
 		for (int i = 1; i < rows-1; i++)
+		{
+			std::vector<bool> rVisible;
+			std::vector<bool> rVisible_top;
+			std::vector<bool> rVisible_bottom;
+			std::vector<bool> rVisible_left;
+			std::vector<bool> rVisible_right;
+
 			for (int j = 1; j < cols-1; j++)
 			{
+
 				bool visible_top = true;
 				bool visible_bottom = true;
 				bool visible_left = true;
@@ -72,17 +110,30 @@ private:
 
 				// right
 				max = data[i][j];
-				for (int jj = j+1; jj < cols; jj++)
+				for (int jj = j + 1; jj < cols; jj++)
 					if (data[i][jj] >= max)
 						visible_right = false;
 
+
+				rVisible_top.push_back(visible_top);
+				rVisible_bottom.push_back(visible_bottom);
+				rVisible_left.push_back(visible_left);
+				rVisible_right.push_back(visible_right);
+				rVisible.push_back(visible_top && visible_bottom && visible_left && visible_right);
+
 				if (visible_top || visible_bottom || visible_left || visible_right)
-					sum++;
+						sum++;
+
 			}
+			mVisible_top.push_back(rVisible_top);
+			mVisible_bottom.push_back(rVisible_bottom);
+			mVisible_left.push_back(rVisible_left);
+			mVisible_right.push_back(rVisible_right);
+			mVisible.push_back(rVisible);
+		}
 
 		// Sum edges
 		sum += 2 * cols + 2 * (rows - 2);
-
 
 		std::cout << "AoC 2022 - Day 08 - Part 1";
 		return std::to_string(sum);
@@ -105,6 +156,7 @@ private:
 
 		for (int i = 1; i < rows - 1; i++)
 		{
+			std::vector<int> rScenic_score;
 			for (int j = 1; j < cols - 1; j++)
 			{
 				int score_top = 0;
@@ -156,15 +208,77 @@ private:
 						score_right++;
 
 				scores.push_back(score_top * score_bottom * score_left * score_right);
-				printf("i:%d, j:%d - %2d %2d %2d %2d score:%2d\n", i, j, score_top, score_left, score_right, score_bottom, score_top * score_bottom * score_left * score_right);
+				rScenic_score.push_back(scores.back());
+				//printf("i:%d, j:%d - %2d %2d %2d %2d score:%2d\n", i, j, score_top, score_left, score_right, score_bottom, score_top * score_bottom * score_left * score_right);
 			}
-			std::cout << std::endl;
+			mScenic_score.push_back(rScenic_score);
+			//std::cout << std::endl;
 		}
+
+		//this->draw();
 
 		std::sort(scores.begin(), scores.end());
 
 		std::cout << "AoC 2022 - Day 08 - Part 2";
 		return std::to_string(scores.back());
 	}
+
+	void draw()
+	{
+
+		std::fstream output_file;
+		std::string path = "C:/Users/dacaz/source/repos/AoC2022/AoC2022/data/output/";
+
+		output_file.open(path + "output08_data.txt", std::fstream::out);
+		std::cout << "data" << std::endl;
+		this->printMatrix(data, output_file);
+		output_file.close();
+
+		output_file.open(path + "output08_UP.txt", std::fstream::out);
+		std::cout << "Up visibility" << std::endl;
+		this->printMatrix(mVisible_top, output_file);
+		output_file.close();
+
+		output_file.open(path + "output08_DOWN.txt", std::fstream::out);
+		std::cout << "Down visibility" << std::endl;
+		this->printMatrix(mVisible_bottom, output_file);
+		output_file.close();
+
+		output_file.open(path + "output08_LEFT.txt", std::fstream::out);
+		std::cout << "Left visibility" << std::endl;
+		this->printMatrix(mVisible_left, output_file);
+		output_file.close();
+
+		output_file.open(path + "output08_RIGHT.txt", std::fstream::out);
+		std::cout << "Right visibility" << std::endl;
+		this->printMatrix(mVisible_right, output_file);
+		output_file.close();
+
+		output_file.open(path + "output08_FULL.txt", std::fstream::out);
+		std::cout << "All directions visibility" << std::endl;
+		this->printMatrix(mVisible, output_file);
+		output_file.close();
+
+		output_file.open(path + "output08_SCORE.txt", std::fstream::out);
+		std::cout << "Scenic Score" << std::endl;
+		this->printMatrix(mScenic_score, output_file);
+		output_file.close();
+	}
+
+	template <typename T>
+	void printMatrix(const 	std::vector<std::vector<T>>& matrix, std::fstream& file)
+	{
+		int rows = matrix.size();
+		int cols = matrix[0].size();
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				file << matrix[i][j] << ",";
+			}
+			file << std::endl;
+		}
+	}
+
 
 };
